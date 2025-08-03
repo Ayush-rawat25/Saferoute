e3635cecd4dabef0fcc34c88d11fb52d31bdfd03
AYUSH RAWAT <rawatayush004@gmail.com>
2025-08-03 18:07:05 +0530

    Redirects

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
+app.use(cors({
+  origin: 'https://saferoute-fronted.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 // Middleware
 app.use(cors());
 app.use(express.json());
