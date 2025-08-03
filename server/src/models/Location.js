18c197512995aa9ed97c4fd23790d9f742b63f03
AYUSH RAWAT <rawatayush004@gmail.com>
2025-08-03 20:23:41 +0530

    location-update

--- server/src/models/Location.js
+++ server/src/models/Location.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-const LocationSchema = new mongoose.Schema({
+const locationSchema = new mongoose.Schema({
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
   isLive: { type: Boolean, default: true },
-  timestamp: { type: Date, default: Date.now }
-}, { timestamps: true });
+  timestamp: { type: Date, default: Date.now },
+}, {
+  timestamps: true,
+});
 
-module.exports = mongoose.model('Location', LocationSchema);
+// Automatically delete after 24 hours
+locationSchema.index({ createdAt: 1 }, { expireAfterSeconds: 86400 });
+
+module.exports = mongoose.model('Location', locationSchema);
