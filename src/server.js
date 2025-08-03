f48240c35ec70b4d86e461bddd0e82f93fcdaf47
AYUSH RAWAT <67622348+Ayush-rawat25@users.noreply.github.com>
2025-08-04 01:36:19 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const routes = require('=../routes/routes');
+const routes = require('../routes/routes');
 require('dotenv').config();
 
 const app = express();
